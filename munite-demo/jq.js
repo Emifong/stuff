20a7350503afa3f663b2421d206a57a97cb6d7de
emelfong <emimelfong@gmail.com>
2020-07-07 23:17:36 -0700

    Add files via upload

---
+++ munite-demo/jq.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+$(document).ready(function(){
+  $('.single-item').slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
+});
+	
